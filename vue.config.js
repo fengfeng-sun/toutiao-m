f7a9ea5eda83eeb5ba5235362dd317cd41e27ef9
DESKTOP-0T0LCLT\XINDONG <1341169790@qq.com>
2022-02-09 14:24:42 +0800

    增加vue.config.js,关闭eslint检查

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// vue.config.js
+
+module.exports = {
+  devServer: {
+    overlay: {
+      // waring: false, //不显示警告
+      // errors: false //不显示错误
+    }
+  },
+  lintOnSave: false // 关闭eslint检查
+}
