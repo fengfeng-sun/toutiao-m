d33b29cfd5dbcfe3f75b23ec1a2e5c3ee902627b
DESKTOP-0T0LCLT\XINDONG <1341169790@qq.com>
2022-02-09 19:30:27 +0800

    增加vant移动端插件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="nav">
     <h1>爱吃肉的瘦子</h1>
+    <!-- <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button> -->
   </div>
   <!-- 路由出口 -->
   <router-view/>
--- src/main.js
+++ src/main.js
@@ -6,4 +6,12 @@ import store from './store'
 // 加载全局样式文件
 import './styles/index.less'
 
-createApp(App).use(store).use(router).mount('#app')
+// 导入vant插件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(Vant)
+app.mount('#app')
