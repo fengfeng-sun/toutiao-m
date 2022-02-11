75443e46912dc469706b7cbeeedd0798c74d4a29
DESKTOP-0T0LCLT\XINDONG <1341169790@qq.com>
2022-02-11 18:49:49 +0800

    配置postcss-pxtorem

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,14 @@
 // postcss.config.js
-
-// module.exports = {
-//     plugins: {
-//         'postcss-pxtorem': {
-//             rootValue: 37.5,
-//             propList: ['*']
-//         }
-//     }
-// }
+// vant设计稿的尺寸是基于宽375设计的，其它尺寸可以将rootValue配置调整
+module.exports = {
+  plugins: {
+    // postcss-pxtorem 插件的版本需要 >= 5.0.0
+    'postcss-pxtorem': {
+      rootValue ({ file }) {
+        console.log('Rem file written.')
+        return file.indexOf('vant') !== -1 ? 37.5 : 75
+      },
+      propList: ['*']
+    }
+  }
+}
