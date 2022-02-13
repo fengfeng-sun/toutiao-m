eb5993d03bebb0b47204c362df2bfdda8b228fff
fengfeng-sun <13918569456@139.com>
2022-02-13 20:25:59 +0800

    登陆页面--准备--布局样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,17 +7,22 @@
         <van-cell-group inset>
             <van-field
             name="手机号"
-            left-icon="phone-o"
+            left-icon="phone-circle-o"
             placeholder="请输入手机号"
             />
             <van-field
             name="验证码"
+            left-icon="comment-circle-o"
             placeholder="请输入验证码"
-            />
+            >
+            <!-- <i slot="left-icon" class="van-badge__wrapper van-icon van-icon-comment-circle-o"></i> -->
+            <template #button>
+                <van-button class="send-sms-btn" round size="mini" type="default">发送验证码</van-button>
+            </template>
+            </van-field>
         </van-cell-group>
-        <!-- <i slot="left-icon" class="van-badge__wrapper van-icon van-icon-tv-o"></i> -->
-        <div style="margin: 16px;">
-            <van-button block type="primary" native-type="submit">
+        <div class="login_btn_wrap">
+            <van-button class="login_btn" block type="primary" native-type="submit">
             登录
             </van-button>
         </div>
@@ -39,8 +44,24 @@ export default {
 
 <style lang="less">
 .login-container{
-    .van-icon-phone-o{
-        font-size: 37px;
+    .van-badge__wrapper{
+        font-size: 50px;
+    }
+    .send-sms-btn{
+        width: 152px;
+        height: 46px;
+        line-height: 46px;
+        font-size: 22px;
+        color: #666;
+        background-color: #ededed;
+    }
+    .login_btn_wrap{
+        padding: 53px 33px;
+        .login_btn{
+            background-color: #6db4fb;
+            border: none;
+        }
     }
 }
+
 </style>
